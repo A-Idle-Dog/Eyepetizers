3d92d3b18797df3e240682f147e200cae8eb6c48
joker <1206897770@qq.com>
2025-07-17 19:16:39 +0800

    测试3

---
+++ module_square/src/main/java/com/example/module_square/adpter/ReAdpter.kt
@@ -0,0 +1,10 @@
+package com.example.module_square.adpter
+
+/**
+ *description:能看小说的app
+ * author 王以飞
+ * email 1206897770@qq.com
+ * date 2025-2-18
+ */
+class ReAdpter {
+}
---
+++ module_square/src/main/java/com/example/module_square/bean/ChildTabBean.kt
@@ -0,0 +1,9 @@
+package com.example.module_square.bean
+
+/**
+ *description:能看小说的app
+ * author 王以飞
+ * email 1206897770@qq.com
+ * date 2025-2-18
+ */
+data class ChildTabBean()
